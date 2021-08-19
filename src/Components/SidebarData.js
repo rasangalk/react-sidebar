4d421e2bc8a8d2dee5bec75f7082b893027b5a3f
rasangalk <rasangalakshith38@gmail.com>
2021-08-19 18:33:25 +0530

    first commit

---
+++ src/Components/SidebarData.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const SidebarData = [
+	{
+		title: "Home",
+		link: "/home",
+	},
+	{
+		title: "Mailbox",
+		link: "/mailbox",
+	},
+	{
+		title: "Analytics",
+		link: "/analytics",
+	},
+	{
+		title: "Dashboard",
+		link: "/dashboard",
+	},
+];
